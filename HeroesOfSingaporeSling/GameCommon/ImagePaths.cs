6748db8ac2f595ab264a278faa6f49f1047356da
danielpetrov <danielpetrov666@mail.bg>
2014-02-11 10:35:34 +0200

    v2

---
+++ HeroesOfSingaporeSling/GameCommon/ImagePaths.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCommon
+{
+    public static class ImagePaths
+    {
+        public static Dictionary<StaticObsticleType, string> StaticObsticleImagePathDict =
+            new Dictionary<StaticObsticleType, string>()
+        {
+            { StaticObsticleType.Tree, "\\Images\\Tree\\small80x100.png" },
+            { StaticObsticleType.Rock, "\\Images\\Stone\\small70x70.png" },
+            { StaticObsticleType.Fence, "\\Images\\Fence\\small60x60.png" },
+            { StaticObsticleType.BorderVertical, "\\Images\\Borders\\BorderVertical.png" },
+            { StaticObsticleType.BorderHorizontal, "\\Images\\Borders\\BorderHorizontal.png" }
+        };
+    }
+}
